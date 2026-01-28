1f7c5553a00983f766faebb7007287e4b086e48a
Fola Azeez <foladevops@gmail.com>
2026-01-28 11:36:01 +0000

    Delete app directory

--- app/app.py
+++
@@ -1,8 +0,0 @@
-def add(a: float, b: float) -> float:
-    return a + b
-
-
-def divide(a: float, b: float) -> float:
-    if b == 0:
-        raise ValueError("b must not be 0")
-    return a / b
--- app/init.py
+++
@@ -1,1 +0,0 @@
-# Makes `tests` a package (not strictly required, but fine).
